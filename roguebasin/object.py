1e69667ba86f0d59785baf268d8d9680a9cb1c26
Eryn Wells <eryn@erynwells.me>
2022-05-06 21:16:00 -0700

    Create a Hero class for the character the player moves; stop using tcod.Color for foreground and background; use tuples instead

--- roguebasin/object.py
+++ roguebasin/object.py
@@ -3,17 +3,17 @@
 
 import tcod
 from .geometry import Point
-from typing import Optional
+from typing import Optional, Tuple
 
 class Entity:
     '''A single-tile drawable entity with a symbol and position.'''
 
     def __init__(self, symbol: str, *,
                  position: Optional[Point] = None,
-                 fg: Optional[tcod.Color] = None,
-                 bg: Optional[tcod.Color] = None):
+                 fg: Optional[Tuple[int, int, int]] = None,
+                 bg: Optional[Tuple[int, int, int]] = None):
         self.position = position if position else Point()
-        self.foreground = fg if fg else tcod.white
+        self.foreground = fg if fg else (255, 255, 255)
         self.background = bg
         self.symbol = symbol
 
@@ -25,3 +25,7 @@ class Entity:
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.symbol}, position={self.position}, fg={self.foreground}, bg={self.background})'
+
+class Hero(Entity):
+    def __init__(self, position: Point):
+        super().__init__('@', position=position, fg=tuple(tcod.white))

fff3260d01bb33246cf7a57c7dc71e24663c2d07
Eryn Wells <eryn@erynwells.me>
2023-02-09 21:00:51 -0800

    Give MapGenerator up_stairs and down_stairs properties

--- erynrl/map/generator/__init__.py
+++ erynrl/map/generator/__init__.py
@@ -1,12 +1,22 @@
+from typing import List
+
 import numpy as np
 
-from ..tile import Empty
 from .corridor import CorridorGenerator
 from .room import RoomGenerator
+from ...geometry import Point
 
 
 class MapGenerator:
     '''Abstract base class defining an interface for generating a map and applying it to a set of tiles.'''
+
+    @property
+    def up_stairs(self) -> List[Point]:
+        raise NotImplementedError()
+
+    @property
+    def down_stairs(self) -> List[Point]:
+        raise NotImplementedError()
 
     def generate(self, tiles: np.ndarray):
         raise NotImplementedError()
@@ -20,6 +30,14 @@ class RoomsAndCorridorsGenerator(MapGenerator):
     def __init__(self, room_generator: RoomGenerator, corridor_generator: CorridorGenerator):
         self.room_generator = room_generator
         self.corridor_generator = corridor_generator
+
+    @property
+    def up_stairs(self) -> List[Point]:
+        return self.room_generator.up_stairs
+
+    @property
+    def down_stairs(self) -> List[Point]:
+        return self.room_generator.down_stairs
 
     def generate(self, tiles: np.ndarray):
         self.room_generator.generate()
